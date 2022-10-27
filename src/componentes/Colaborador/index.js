a5048571defe86a4ba39cc07e47b78274dbf1ab9
O-Theu <113058396+O-Theu@users.noreply.github.com>
2022-10-27 16:28:15 -0300

    first commit

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,32 @@
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {FcLike, FcLikePlaceholder} from 'react-icons/fc';
+import './colaborador.css';
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar}) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    return (<div className="colaborador">
+        <AiFillCloseCircle 
+            size={25} 
+            className='deletar' 
+            onClick={() => aoDeletar(colaborador.id)}
+        />
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
+        </div>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                    ? <FcLike size={25} onClick={favoritar}/> 
+                    : <FcLikePlaceholder size={25} onClick={favoritar} /> 
+                }
+            </div>
+        </div>
+    </div>)
+}
+
+export default Colaborador
